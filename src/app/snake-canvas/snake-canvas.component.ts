dabe5938f45d481d2a341904ad993a2b97be1aae
Grigory.Tukmachev <grigory@clearscale.net>
2017-04-03 23:23:05 +0700

    constants

--- src/app/snake-canvas/snake-canvas.component.ts
+++ src/app/snake-canvas/snake-canvas.component.ts
@@ -6,62 +6,65 @@ import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
   styleUrls: ['./snake-canvas.component.css']
 })
 export class SnakeCanvasComponent implements OnInit {
+  static startRadius = 15;
+  static stepLength = 7;
 
   @ViewChild('myCanvas') canvasRef: ElementRef;
 
-  constructor() {}
+  constructor () {
+  }
 
-  ngOnInit() {
+  ngOnInit () {
     const ctx: CanvasRenderingContext2D =
       this.canvasRef.nativeElement.getContext('2d');
 
     for (let i = 0; i < 20; i++) {
-      this.drawCircle(ctx, 100 + i * 20, 100, 20);
+      this.drawCircle(ctx, 100 + i * SnakeCanvasComponent.stepLength, 100);
     }
 
 
-/*
-    // Draw the clip path that will mask everything else
-    // that we'll draw later.
-    ctx.beginPath();
-    ctx.moveTo(250, 60);
-    ctx.lineTo(63.8, 126.4);
-    ctx.lineTo(92.2, 372.6);
-    ctx.lineTo(250, 460);
-    ctx.lineTo(407.8, 372.6);
-    ctx.lineTo(436.2, 126.4);
-    ctx.moveTo(250, 104.2);
-    ctx.lineTo(133.6, 365.2);
-    ctx.lineTo(177, 365.2);
-    ctx.lineTo(200.4, 306.8);
-    ctx.lineTo(299.2, 306.8);
-    ctx.lineTo(325.2, 365.2);
-    ctx.lineTo(362.6, 365.2);
-    ctx.lineTo(250, 104.2);
-    ctx.moveTo(304, 270.8);
-    ctx.lineTo(216, 270.8);
-    ctx.lineTo(250, 189);
-    ctx.lineTo(284, 270.8);
-    ctx.clip('evenodd');
+    /*
+     // Draw the clip path that will mask everything else
+     // that we'll draw later.
+     ctx.beginPath();
+     ctx.moveTo(250, 60);
+     ctx.lineTo(63.8, 126.4);
+     ctx.lineTo(92.2, 372.6);
+     ctx.lineTo(250, 460);
+     ctx.lineTo(407.8, 372.6);
+     ctx.lineTo(436.2, 126.4);
+     ctx.moveTo(250, 104.2);
+     ctx.lineTo(133.6, 365.2);
+     ctx.lineTo(177, 365.2);
+     ctx.lineTo(200.4, 306.8);
+     ctx.lineTo(299.2, 306.8);
+     ctx.lineTo(325.2, 365.2);
+     ctx.lineTo(362.6, 365.2);
+     ctx.lineTo(250, 104.2);
+     ctx.moveTo(304, 270.8);
+     ctx.lineTo(216, 270.8);
+     ctx.lineTo(250, 189);
+     ctx.lineTo(284, 270.8);
+     ctx.clip('evenodd');
 
-    // Draw 50,000 circles at random points
-    ctx.beginPath();
-    ctx.fillStyle = '#DD0031';
-    for (let i=0 ; i < 50000 ; i++) {
-      let x = Math.random() * 500;
-      let y = Math.random() * 500;
-      ctx.moveTo(x, y);
-      ctx.arc(x, y, 1, 0, Math.PI * 2);
-    }
-    ctx.fill();
-*/
+     // Draw 50,000 circles at random points
+     ctx.beginPath();
+     ctx.fillStyle = '#DD0031';
+     for (let i=0 ; i < 50000 ; i++) {
+     let x = Math.random() * 500;
+     let y = Math.random() * 500;
+     ctx.moveTo(x, y);
+     ctx.arc(x, y, 1, 0, Math.PI * 2);
+     }
+     ctx.fill();
+     */
   }
 
 
-  private drawCircle(ctx: CanvasRenderingContext2D, x: number, y: number, r: number) {
+  private drawCircle (ctx: CanvasRenderingContext2D, x: number, y: number) {
     ctx.beginPath();
     ctx.fillStyle = '#31ddbe';
-    ctx.arc(x, y, r, 0, Math.PI * 2);
+    ctx.arc(x, y, SnakeCanvasComponent.startRadius, 0, Math.PI * 2);
     ctx.fill();
   }
 
