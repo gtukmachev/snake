2db30496210f9c06ff1c4e9c9dfca6dd0f82c546
Grigory.Tukmachev <grigory@clearscale.net>
2017-04-04 10:04:35 +0700

    fixed length snake is ready

--- src/app/snake-canvas/snake-canvas.component.ts
+++ src/app/snake-canvas/snake-canvas.component.ts
@@ -1,88 +1,173 @@
-import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
+import {Component, ElementRef, HostListener, OnDestroy, OnInit, ViewChild} from '@angular/core';
+import {Observable, Subscription} from "rxjs";
 
 @Component({
   selector: 'app-snake-canvas',
   templateUrl: './snake-canvas.component.html',
   styleUrls: ['./snake-canvas.component.css']
 })
-export class SnakeCanvasComponent implements OnInit {
+export class SnakeCanvasComponent implements OnInit, OnDestroy {
   static startRadius = 15;
-  static stepLength = 7;
 
   @ViewChild('myCanvas') canvasRef: ElementRef;
 
+  private xSize: number = 1000;
+  private ySize: number = 800;
+
+  private stepLength = 2;
+  private stayCloneAfter = 7;
+  private cycleFrameCounter = 0;
+  private timerDelay = 20;
+
+
   protected running: boolean = false;
+
+  protected lifeCounter: number = 0;
+  protected snakeElements: SnakeElement[] = [];
+
+  protected mouseDown = false;
+  protected mouseEvent: MouseEvent;
+  protected mx = 0;
+  protected my = 0;
+
+  protected dx = 0;
+  protected dy = 0;
+
+  private gameTimer: Subscription;
 
   constructor () {
   }
 
   ngOnInit () {
-    const ctx: CanvasRenderingContext2D =
-      this.canvasRef.nativeElement.getContext('2d');
-
-    for (let i = 0; i < 20; i++) {
-      this.drawCircle(ctx, 100 + i * SnakeCanvasComponent.stepLength, 100);
-    }
 
 
-    /*
-     // Draw the clip path that will mask everything else
-     // that we'll draw later.
-     ctx.beginPath();
-     ctx.moveTo(250, 60);
-     ctx.lineTo(63.8, 126.4);
-     ctx.lineTo(92.2, 372.6);
-     ctx.lineTo(250, 460);
-     ctx.lineTo(407.8, 372.6);
-     ctx.lineTo(436.2, 126.4);
-     ctx.moveTo(250, 104.2);
-     ctx.lineTo(133.6, 365.2);
-     ctx.lineTo(177, 365.2);
-     ctx.lineTo(200.4, 306.8);
-     ctx.lineTo(299.2, 306.8);
-     ctx.lineTo(325.2, 365.2);
-     ctx.lineTo(362.6, 365.2);
-     ctx.lineTo(250, 104.2);
-     ctx.moveTo(304, 270.8);
-     ctx.lineTo(216, 270.8);
-     ctx.lineTo(250, 189);
-     ctx.lineTo(284, 270.8);
-     ctx.clip('evenodd');
-
-     // Draw 50,000 circles at random points
-     ctx.beginPath();
-     ctx.fillStyle = '#DD0031';
-     for (let i=0 ; i < 50000 ; i++) {
-     let x = Math.random() * 500;
-     let y = Math.random() * 500;
-     ctx.moveTo(x, y);
-     ctx.arc(x, y, 1, 0, Math.PI * 2);
-     }
-     ctx.fill();
-     */
   }
 
-  private paint() {
-    // Check that we're still running.
-    if (!this.running) {
-      return;
+  private onMouse(event: MouseEvent) {
+    if (!this.running) return;
+
+    this.mouseEvent = event;
+    this.mx = event.layerX;
+    this.my = event.layerY;
+
+    const head = this.snakeElements[this.snakeElements.length - 1];
+
+    const vectorX = this.mx-head.x;
+    const vectorY = this.my-head.y;
+
+    const len = Math.sqrt( vectorX*vectorX + vectorY*vectorY );
+
+    this.dx = ( vectorX / len ) * this.stepLength;
+    this.dy = ( vectorY / len ) * this.stepLength;
+
+  }
+
+  @HostListener('mousemove', ['$event']) onMouseMove(event: MouseEvent) {
+    if(this.mouseDown) this.onMouse(event);
+  }
+
+  @HostListener('mousedown', ['$event']) onMouseDown(event: MouseEvent) {
+    this.mouseDown = true; this.onMouse(event);
+  }
+
+  @HostListener('mouseup', ['$event']) onMouseUp(event) {
+    this.mouseDown = false;
+  }
+
+  private gameStep(): void {
+    if (!this.running) { return; }
+
+    this.cycleFrameCounter++;
+
+    let head = this.snakeElements[this.snakeElements.length-1];
+    if (this.cycleFrameCounter >= this.stayCloneAfter) {
+      this.cycleFrameCounter = 0;
+      this.lifeCounter++;
+      head = new SnakeElement(head.x, head.y, this.lifeCounter + 10 );
+      this.snakeElements.push( head );
     }
 
-    const ctx: CanvasRenderingContext2D =
-      this.canvasRef.nativeElement.getContext('2d');
+    head.x += this.dx;
+    head.y += this.dy;
 
+    if (this.snakeElements[0].whenDie < this.lifeCounter) {
+      this.snakeElements = this.snakeElements.slice(1, this.snakeElements.length);
+    }
+  }
 
+  private paint(): void {
+    // Check that we're still running.
+    if (!this.running) return;
+
+    const ctx: CanvasRenderingContext2D = this.canvasRef.nativeElement.getContext('2d');
+
+    // Draw background (which also effectively clears any previous drawing)
+    ctx.fillStyle = '#fdffe3';
+    ctx.fillRect(0, 0, this.xSize, this.ySize);
+
+    this.drawSnake(ctx);
 
     requestAnimationFrame(() => this.paint());
   }
 
-  private drawCircle (ctx: CanvasRenderingContext2D, x: number, y: number) {
+  private drawSnake (ctx: CanvasRenderingContext2D) {
+    for (let i = 0; i < this.snakeElements.length; i++) {
+      const element = this.snakeElements[i];
+
+      if (i == this.snakeElements.length - 1) {
+        this.drawCircle(ctx, element.x, element.y, '#3a599a')
+      } else {
+        this.drawCircle(ctx, element.x, element.y, '#29b396')
+      }
+    }
+
+
+  }
+
+  private drawCircle (ctx: CanvasRenderingContext2D, x: number, y: number, fillStyle: string | CanvasGradient | CanvasPattern) {
     ctx.beginPath();
-    ctx.fillStyle = '#31ddbe';
+    ctx.fillStyle = fillStyle;
     ctx.arc(x, y, SnakeCanvasComponent.startRadius, 0, Math.PI * 2);
     ctx.fill();
   }
 
-  public changeRun() { this.running = !this.running; }
+  public changeRun () {
+    this.running = !this.running;
+    if (this.running) {
+      this.snakeElements = [new SnakeElement(30,100, 10)];
+      this.gameTimer = Observable.timer(500, this.timerDelay)
+        .subscribe(
+          () => this.gameStep()
+        );
+      this.paint();
+    } else {
+      this.gameTimer.unsubscribe();
+    }
+  }
 
+
+  ngOnDestroy (): void {
+    this.gameTimer.unsubscribe();
+  }
+
+  protected mouseJson(): string {
+
+    return (this.mouseEvent == null) ? 'null' : JSON.stringify(this.mouseEvent)
+
+  }
+
+}
+
+
+class SnakeElement {
+
+  public x: number;
+  public y: number;
+  public whenDie: number;
+
+  constructor (x: number, y: number, whenDie: number) {
+    this.x = x;
+    this.y = y;
+    this.whenDie = whenDie;
+  }
 }
