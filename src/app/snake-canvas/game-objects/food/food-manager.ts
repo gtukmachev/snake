48e7c842767462ae25131bd813f38ec6462e21a2
Grigory.Tukmachev <grigory@clearscale.net>
2017-04-06 15:06:33 +0700

    refactoring

--- src/app/snake-canvas/game-objects/food/food-manager.ts
+++ src/app/snake-canvas/game-objects/food/food-manager.ts
@@ -1,13 +1,14 @@
 import {GameField} from "../game-field";
 import {ManagerGameObject} from "../manager-game-object";
 import {Food} from "./food";
+import {TimeCounter} from "../time-counter";
 
 
 export class FoodManager extends ManagerGameObject {
 
   foodCount: number = 0;
 
-  lastMsAdded: number = 0;
+  timeCounter: TimeCounter = new TimeCounter(3000); // for every 3 sec
 
   constructor (field: GameField) {
     super(field);
@@ -15,12 +16,9 @@ export class FoodManager extends ManagerGameObject {
 
 
   beforeTurn (): void {
-    const ms = new Date().getTime();
-    if ((this.lastMsAdded + 3000) < ms ) {
-      if (this.foodCount <= 3) {
-        this.makeFood();
-        this.lastMsAdded = ms;
-      }
+    if (this.timeCounter.isItTime()) {
+      this.timeCounter.fixLastChecking();
+      if (this.foodCount <= 3) this.makeFood();
     }
   }
 
@@ -30,7 +28,7 @@ export class FoodManager extends ManagerGameObject {
       this.field,
       Math.round(Math.random() * (this.field.size.x - 20)) + 10,
       Math.round(Math.random() * (this.field.size.y - 20)) + 10,
-      Math.round(Math.random() * 10) + 10,
+      Math.round(Math.random() * 8) + 5,
       '#c277ff'
     );
 
